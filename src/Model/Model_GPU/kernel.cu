b4206e74704f5a955dd4abe374ef41f69140ba11
martinluza <martin.luzardo1@gmail.com>
2026-03-04 14:29:41 +0100

    first commit gpu

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -7,12 +7,51 @@
 
 __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	for (int i = 0; i < n_particles; i ++)
+    {
+        for (int j = 0; j < n_particles; j++)
+        {
+            if(i != j)
+            {
+                const float diffx = particles.x[j] - particles.x[i];
+                const float diffy = particles.y[j] - particles.y[i];
+                const float diffz = particles.z[j] - particles.z[i];
+
+                float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+                if (dij < 1.0)
+                {
+                    dij = 10.0;
+                }
+                else
+                {
+                    dij = std::sqrt(dij);
+                    dij = 10.0 / (dij * dij * dij);
+                }
+
+                accelerationsx[i] += diffx * dij * initstate.masses[j];
+                accelerationsy[i] += diffy * dij * initstate.masses[j];
+                accelerationsz[i] += diffz * dij * initstate.masses[j];
+            }
+        }
+    }
 }
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, int n_particles)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+	for (int i = 0; i < n_particles; i++)
+	{
+		velocitiesx[i] += accelerationsx[i] * 2.0f;
+		velocitiesy[i] += accelerationsy[i] * 2.0f;
+		velocitiesz[i] += accelerationsz[i] * 2.0f;
+		particles.x[i] += velocitiesx   [i] * 0.1f;
+		particles.y[i] += velocitiesy   [i] * 0.1f;
+		particles.z[i] += velocitiesz   [i] * 0.1f;
+	}
 
 }
 
